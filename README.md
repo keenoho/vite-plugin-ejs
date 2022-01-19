7963d7e2d8230e72bf9f1404f1e9f6471e4d8778
leiwenwei <1207893654@qq.com>
2022-01-19 14:17:52 +0800

    add

---
+++ README.md
@@ -0,0 +1,18 @@
+# vite-plugin-ejs
+
+## usage
+vite.config.js
+```
+import ejsPlugin from './ejs-plugin';
+
+export default {
+    ...other config
+
+    plugins: [
+      ejsPlugin(),
+      ...other plugin
+    ],
+
+    ...other config
+}
+```
---
+++ index.js
@@ -0,0 +1,41 @@
+const ejs = require('ejs');
+
+function ViteEjsPlugin(data = {}, ejsOption = {}) {
+  let config;
+
+  return {
+    name: 'vite-plugin-ejs',
+
+    configResolved(resolvedConfig) {
+      config = resolvedConfig;
+    },
+
+    transformIndexHtml: {
+      enforce: 'pre',
+      transform(html) {
+        try {
+          if (typeof data === 'function') data = data(config);
+
+          html = ejs.render(
+            html,
+            {
+              command: config.command,
+              mode: config.mode,
+              NODE_ENV: process.env.NODE_ENV || config.mode,
+              ...data,
+            },
+            ejsOption
+          );
+        } catch (e) {
+          return e.message;
+        }
+
+        return html;
+      },
+    },
+  };
+}
+
+ViteEjsPlugin.default = ViteEjsPlugin;
+
+module.exports = ViteEjsPlugin;
